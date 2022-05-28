7d87dbc4e6561b4cea384d58a78225d27e8daa5b
shiroe66 <shiroeqky@gmail.com>
2022-05-28 14:46:35 +0500

    feat: html markdown n install socket.io

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express()
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+
+const users = []
 
 app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
+})
+
+io.on("connection", (socket) => {
+  console.log(socket)
+  users.push(socket)
 })
 
 app.listen("3000", () => {
